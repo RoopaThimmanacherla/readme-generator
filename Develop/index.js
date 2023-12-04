4c6805a2adc9418a5e55950719b5659dbb4b45dc
Roopa Thimmanacherla <roopareddy90@gmail.com>
2023-12-04 10:30:21 -0800

    added license details in js file

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown.js");
-const fs = require("fs");
+const { writeFile } = require("fs").promises;
 
 // TODO: Create an array of questions for user input
 const questions = () => {
